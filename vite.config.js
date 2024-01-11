e91a82ef128af726cd5318e3d9809f07838cf5b3
Jacob Jhohnson <jjskunk0503@gmail.com>
2024-01-10 21:06:02 -0500

    new vite congit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Existing chunks...
+          vendor: ["react", "react-dom", "@chakra-ui/react"],
+          // Separate chunk for Firebase Authentication
+          firebaseAuth: ["firebase/auth", "firebase/app", "firebase/firestore"],
+          // Add more chunks as needed
+        },
+      },
+    },
+  },
+});
